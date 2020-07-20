f4357b053718a3ea5fd0849bc022097f298fd65d
Katie Le <lethikatie@gmail.com>
2020-07-20 22:39:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,5 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = )
     @name = "Fido"
     @breed = "Pug"
   end 
-
-  def breed=(breed= "Mutt")
-    @breed = "Mutt"
-  end 
-  
-  def breed
-    @breed
-  end 
-end 
